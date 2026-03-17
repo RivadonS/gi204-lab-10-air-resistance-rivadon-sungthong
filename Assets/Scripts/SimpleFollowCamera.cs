51e1040fe04216a8ad31ccd12e5c95e3b1b559a8
RivadonS <rivadon.sung@bumail.net>
2026-03-17 16:00:16 +0700

    Camera

--- Assets/Scripts/SimpleFollowCamera.cs
+++ Assets/Scripts/SimpleFollowCamera.cs
@@ -1,18 +1,18 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SimpleFollowCamera : MonoBehaviour
 {
     public Transform target;
-    public Vector3 offset = new Vector3 (0, 4, -49);
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Vector3 offset = new Vector3 (0, 3, -8);
+    public float smoothSpeed = 5f;
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        Vector3 despiredPosition = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, despiredPosition, smoothSpeed * Time.deltaTime);
+
+        Quaternion targetRotation = Quaternion.LookRotation(target.position - transform.position);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, smoothSpeed * Time.deltaTime);
     }
 }
